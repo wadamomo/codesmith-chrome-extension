34f85afd9efbee31ea3a6f3dcad614acfca96272
William Adamowicz <william.adamowicz@gmail.com>
2018-04-25 21:31:50 -0700

    adds manifest

---
+++ questions.js
@@ -0,0 +1,3 @@
+var image = '<img src="https://images.unsplash.com/photo-1484244233201-29892afe6a2c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2db8de5b9f0478ba59dfd9d61f4d64fe&auto=format&fit=crop&w=2550&q=80" height="80%" width="80%">'
+
+console.log('hello hi hi')
